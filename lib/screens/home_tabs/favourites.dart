1456a12f9e75ac1134dc7704b63aacefed34f30c
Haroki <haroki@localhost.localdomain>
2020-07-20 10:27:31 +0200

    add a card with image and bookmark icon

--- lib/screens/home_tabs/favourites.dart
+++ lib/screens/home_tabs/favourites.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class favourites extends StatefulWidget {
   @override
@@ -7,9 +8,53 @@ class favourites extends StatefulWidget {
 class _favouritesState extends State<favourites> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.amber,
+   return ListView.builder(
+       itemBuilder: (context,position){
+         return Card(
+           child: Container(
+             padding: EdgeInsets.all(16),
+             child: Column(
+               children: <Widget>[
 
+                 _authorRow(),
+                 _newsRow(),
+               ],
+             ),
+           ),
+         );
+       },
+     itemCount: 20,
+   );
+  }
+  Widget _authorRow(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: ExactAssetImage('assets/images/home.jpeg'),
+                  fit: BoxFit.cover
+                )
+              ),
+            )
+          ],
+        ),
+        IconButton(
+          icon: Icon(Icons.bookmark_border),
+          onPressed: (){},
+        ),
+
+      ],
     );
+
+  }
+  Widget _newsRow(){
+  return Container();
   }
 }
