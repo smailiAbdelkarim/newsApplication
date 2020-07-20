855f2881ee8ae2c8c591fddb1a4a72a2ea40eef5
Haroki <haroki@localhost.localdomain>
2020-07-20 12:50:06 +0200

    add titles in the navigationDrawer using LisTiles

--- lib/shared_ui/navigation_drawer.dart
+++ lib/shared_ui/navigation_drawer.dart
@@ -6,10 +6,37 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+  List<String> drawerTitles = [
+    "Explore",
+    "Headline News",
+    "Read Later",
+    "Videos",
+    "photos",
+    "Setting",
+    "Layout"
+
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Drawer(
+        child: Padding(
+          padding: EdgeInsets.only(top:80),
+          child: ListView.builder(
+               itemCount: 7,
+              itemBuilder: (context,index){
+                 return ListTile(
+                   onTap: (){},
+                   trailing: Icon(Icons.chevron_right,color: Colors.grey.shade400,),
+                 title: Text(drawerTitles[index],style: TextStyle(
+                   fontSize: 22,
+                   color: Colors.grey.shade700,
+                 ),),
 
+                 );
+              },
+          ),
+        ),
     );
   }
 }
